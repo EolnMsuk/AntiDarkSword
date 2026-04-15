ecfe08ae48d4a7da284da8af12315dbb2cf28752
Eoln Msuk <90252497+EolnMsuk@users.noreply.github.com>
2026-04-15 10:20:44 -0700

    Update ADSLogging.h

--- ADSLogging.h
+++ ADSLogging.h
@@ -1,5 +1,10 @@
 // ADSLogging.h
 #import <Foundation/Foundation.h>
 
-// Always log file, function, and line number (Debug + Release)
-#define ADSLog(fmt, ...) NSLog(@"[AntiDarkSword] %s [Line %d] " fmt, __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#ifdef DEBUG
+    // In debug builds, log the file name, function name, and line number
+    #define ADSLog(fmt, ...) NSLog(@"[AntiDarkSword] %s [Line %d] " fmt, __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
+#else
+    // In release builds suppress all logging to avoid leaking operational details
+    #define ADSLog(fmt, ...) ((void)0)
+#endif
