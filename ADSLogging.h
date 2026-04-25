759e4cff0b54a3c357230f8c1214dcf8bf5096a0
EolnMsuk <mblend27@gmail.com>
2026-04-24 22:20:13 -0700

    Enforce .gitignore and clean tracked files

--- ADSLogging.h
+++
@@ -1,7 +0,0 @@
-#import <Foundation/Foundation.h>
-
-#ifdef DEBUG
-    #define ADSLog(fmt, ...) NSLog(@"[AntiDarkSword] %s [Line %d] " fmt, __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__)
-#else
-    #define ADSLog(fmt, ...) ((void)0)
-#endif
